52c9945f84509987608afa5545401df42fc394ef
BAITAP BUOI_02 <dangtrongluong4@gmail.com>
2025-06-19 16:14:26 +0700

    update

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -15,7 +15,7 @@ public class HelloServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, World, I am a servlet, 10.06.2025!</h1>");
+        out.println("<h1>Hello, World, I am a servlet,chú ý nhánh của dangluong-17.06.2025!</h1>");
         out.println("<p>Neu ban thay cai nya nghia la ban da chay duoc</p>");
     }
 }
